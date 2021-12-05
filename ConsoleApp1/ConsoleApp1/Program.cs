0cf7a537530cf0fce84d2198100286f88a5c3857
[parvinFP] <[parvinfp@code.edu.az]>
2021-12-05 22:26:23 +0300

    less10

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ConsoleApp1
+{
+    public class less10
+    {
+        public static void Main()
+        {
+            strclass str1 = new strclass();
+            string strres=str1.str01("salam dunya!");
+            Console.WriteLine(strres);    
+
+
+
+
+
+        }
+}
+}
---
+++ ConsoleApp1/ConsoleApp1/strclass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class strclass
+    {
+        public string str01(string word)
+        {
+            
+           string [] word1 = word.Split(" ");
+
+            StringBuilder sb = new StringBuilder(); 
+            foreach (string word2 in word1)
+            {
+                sb.Append(str2revers(word2)+" ");
+            }
+
+            return sb.ToString();
+            
+        }
+        string str2revers(string wordtex)
+        {
+            StringBuilder sb = new StringBuilder(); 
+            for (int i = wordtex.Length - 1; i >= 0; i--)
+            {
+                sb.Append(wordtex[i].ToString());
+            }
+
+            return sb.ToString();  
+        }
+    }
+}
